58e71e6b551f5f268c93c60e7ec892f20421510d
Grisha Bogatyrev <jamesofin@gmail.com>
2023-04-02 13:44:50 +0500

    first commit

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Кто разработал язык программирования JavaScript?');
+alert("За сколько дней был разработан JavaScript?");
+alert("В каком году появился JavaScript?");
+alert("JavaScript - это тоже самое что и Java?");
+alert("Как называется спецификация JavaScript?");
